8e0d7d20069d57db94cbcfb205060869a4cdc5da
Giuseppe Naponiello <beppenapo@arc-teamcom>
2019-02-25 11:47:12 +0100

    google analytics

--- inc/css.php
+++ inc/css.php
@@ -2,3 +2,12 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans|Titillium+Web|Montserrat" rel="stylesheet">
 <link rel="stylesheet" href="css/main.css">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135119466-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-135119466-1');
+</script>
