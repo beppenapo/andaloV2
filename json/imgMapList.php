5e10154a7cbb26b12d215ff4ef72004109d102bf
Giuseppe Naponiello <beppenapo@arc-team.com>
2022-12-26 11:09:23 +0100

    mapList pulizia

--- json/imgMapList.php
+++ json/imgMapList.php
@@ -8,21 +8,4 @@ foreach ($_GET['scheda'] as $foto) {
   array_push($list,$res);
 }
 echo json_encode($list);
-// $imgList = $obj->simple("
-// SELECT
-//   scheda.id,
-//   scheda.dgn_dnogg,
-//   scheda.dgn_numsch,
-//   file.path
-// FROM
-//   aree_scheda,
-//   file,
-//   scheda
-// WHERE
-//   aree_scheda.id_scheda = scheda.id AND
-//   file.id_scheda = scheda.id and
-//   aree_scheda.id_area = ".$_GET['area']."
-// group by scheda.id, scheda.dgn_dnogg,scheda.dgn_numsch, file.path;
-// ");
-// echo json_encode($imgList);
 ?>
